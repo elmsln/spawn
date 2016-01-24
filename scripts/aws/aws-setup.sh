f37d6cb7b38e9939bd0c5adce88fcbbf70efaf67
Brad Fisher <bradallenfisher@gmail.com>
2016-01-24 18:44:38 -0500

    Create aws-setup.sh

---
+++ scripts/aws/aws-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This is the code needed to setup your environment to be able to connect to your AWS Account and use the AWS CLI. 
+# This is not specific to the EC2 instances that will be built, rather it is what connects the user running the commands
+# to the entire console and suite of AWS services.
+
+# You will want to make sure that you have pip installed before doing anything 
+# Run as root
+curl -O https://bootstrap.pypa.io/get-pip.py
+
+# Install pip as root
+python get-pip.py
+
+# Install AWS
+pip install awscli
+
+# Now we can configure... but we will need keys and stuff this will prompt you for your IAM user account's credentials. 
+aws configure
