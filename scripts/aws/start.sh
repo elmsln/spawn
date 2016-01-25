2e1cb49206c9345a1ee4fa8198eb4861958d41e6
Brad Fisher <bradallenfisher@gmail.com>
2016-01-24 19:43:33 -0500

    Update start.sh

--- scripts/aws/start.sh
+++ scripts/aws/start.sh
@@ -2,23 +2,24 @@
 ######### NOT TESTED THIS IS JUST SCAFFOLDING #########################
 
 # basic config variables needed to run an aws ec2 instance setup
-keyname=input1
-mySecurityGroup=input2
-securityDescription=input3
+NEW_UUID="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
+keyName="spawn-$NEW_UUID"
+defaultSecurityGroup="spawn-http"
+securityDescription="baseline security group for allowing traffic on port 80, 3306 over TCP"
 sgId="$(aws ec2 create-security-group --group-name $mySecurityGroup --description "$securityDescription" --query 'GroupId')"
-amiId=input4
-count=input5
-iType=input6
+amiId="ami-60b6c60a" # this is the default Amazon AMI image
+count=1
+iType="t2.micro" # defaults to free tier
 
 # Create key pair and capture the output for the .pem file 
-aws ec2 create-key-pair --key-name $key-name --query 'KeyMaterial' > $key-name.pem
+aws ec2 create-key-pair --key-name $keyName --query 'KeyMaterial' > $keyName.pem
 
 # change permissions to read only for user
 chmod 400 $key-name.pem
 
 # Create First Security Group and Catch Id you will need it to create an instance. The variable is set in basic config and will be
 # used when spawing the instance.
-aws ec2 create-security-group --group-name $mySecurityGroup --description "$securityDescription" --query 'GroupId' 
+aws ec2 create-security-group --group-name $defaultSecurityGroup --description "$securityDescription" --query 'GroupId' 
 
 # Create a new instance to run stuff on
 aws ec2 run-instances --image-id amiId --count $count --instance-type $iType --key-name keyname --security-group-ids $sgId
