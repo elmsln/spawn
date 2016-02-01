758c82c636ba93035b850be554eb0fa9c68167d7
Brad Fisher <bradallenfisher@gmail.com>
2016-02-01 10:38:53 -0500

    Update root.sh

--- scripts/install/root.sh
+++ scripts/install/root.sh
@@ -146,6 +146,8 @@ chown -R vagrant:vagrant /var/www/html
 
 # Move Drupal into place
 cp /vagrant/drupal-7/ /var/www/html/ -rf
+chown -R vagrant:vagrant /var/www/html/drupal-7
+chown -R apache:apache /var/www/html/drupal-7/sites/default
 # Fix date timezone errors
 sed -i 's#;date.timezone =#date.timezone = "America/New_York"#g' /etc/php.ini
 
