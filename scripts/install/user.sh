9a44f12808e8d35722a2535de6b65a5037a9ab20
Brad Fisher <bradallenfisher@gmail.com>
2016-02-02 20:32:53 -0500

    Update user.sh

--- scripts/install/user.sh
+++ scripts/install/user.sh
@@ -1,4 +1,4 @@
 composer global require drush/drush:7.*
 echo 'export PATH="$HOME/.config/composer/vendor/bin:$PATH"' >> $HOME/.bashrc
-echo 'alias sync="sudo rsync -zavr --delete /var/www/html/drupal-7/ /vagrant/drupal-7/"' >> $HOME/.bashrc
+echo 'alias sync="sudo rsync -zavr --delete --exclude sites/default/* /var/www/html/drupal-7/ /vagrant/drupal-7/"' >> $HOME/.bashrc
 source $HOME/.bashrc
