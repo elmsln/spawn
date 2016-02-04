b1be33895d864e1660ea9381a307e77e6ffb8629
Brad Fisher <bradallenfisher@gmail.com>
2016-02-04 13:30:38 -0500

    Update user.sh

--- scripts/install/user.sh
+++ scripts/install/user.sh
@@ -1,8 +1,3 @@
 composer global require drush/drush:7.*
 echo 'export PATH="$HOME/.config/composer/vendor/bin:$PATH"' >> $HOME/.bashrc
 echo 'alias sync="sudo rsync -zavr --delete --exclude sites/default/* /var/www/html/drupal-7/ /vagrant/drupal-7/"' >> $HOME/.bashrc
-
-\curl -sSL https://get.rvm.io | bash -s stable
-source /home/vagrant/.rvm/scripts/rvm
-rvm get head
-source $HOME/.bashrc
